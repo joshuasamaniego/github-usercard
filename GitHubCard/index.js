4f5fce3fb9a2a10207ee4c1bc055728f5e3889ca
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-10-29 16:29:01 -0600

    Adds axios import

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,9 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+import axios from 'axios';
+axios
+  .get("https://api.github.com/users/joshuasamaniego");
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
